83b6b195697fcfb11db20fbb1473b32448ea91f7
digitartdesigns <florin@digitartdesigns.com>
2016-10-25 10:46:35 -0700

    nwe

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
-var url = "http://dreamcatcherfinancing.com/?app";     
+var url = "http://dreamcatcherfinancing.com/?app"; 
+alert(url);
 $(location).attr('href',url);
 });
